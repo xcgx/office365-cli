4f1d28b2189e153bc3441b9c82c0dc9596021e68
xcgx <36234354+xcgx@users.noreply.github.com>
2022-04-13 19:17:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import subprocess
 
-out_bytes = subprocess.check_output('npm install -g @pnp/cli-microsoft365', shell=True)
+out_bytes = subprocess.check_output('m365 login  --authType password --userName info@dakfpv.com.au --password Jeremy_2009', shell=True)
 
 print(out_bytes)
