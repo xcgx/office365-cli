b7bea77e10812f400431dc5068a4e56be244d037
xcgx <36234354+xcgx@users.noreply.github.com>
2022-04-13 23:23:05 +0800

    Update and rename test13.py to test14.py

--- test14.py
+++ test14.py
@@ -15,8 +15,19 @@ def run():
         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' 开始运行')
         for acc in accs:
             try:
-                print(acc[0] + ':' + acc[1])
-                print(get_status(acc) + '\n')
+                if get_status(acc) == '密码正确':
+                    good = acc
+                    with open('./good.txt', 'a', encoding='utf-8') as fa:
+                        fa.write(good)
+                        fa.write('\n')
+                elif get_status(acc) == '需要添加二验':
+                    manual = acc
+                    with open('./manual.txt', 'a', encoding='utf-8') as fa:
+                        fa.write(manual)
+                        fa.write('\n')
+                else:
+                    pass
+
             except Exception as e:
                 pass
     else:
@@ -35,12 +46,16 @@ def get_status(acc):
         return '密码错误'
     elif '53003' in str(status):
         return 'IP限制'
-    elif '53003' in str(status):
+    elif '50057' in str(status):
         return '管理ban'
-    elif '53003' in str(status):
+    elif '50059' in str(status):
         return '被删号'
-    elif '53003' in str(status):
+    elif '50053' in str(status):
         return '被暴力破解，自动ban'
+    elif '90019' in str(status):
+        return '账号错误'
+    elif '50079' in str(status):
+        return '需要添加二验'
     else:
         print(status)
         return '未知错误'
