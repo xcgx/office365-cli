a708629bf4f8071d42d49041565fb0b3259cf677
xcgx <36234354+xcgx@users.noreply.github.com>
2022-04-17 19:34:35 +0800

    Update azure-cli-install-CentOS.sh

--- azure-cli-install-CentOS.sh
+++ azure-cli-install-CentOS.sh
@@ -8,4 +8,4 @@ baseurl=https://packages.microsoft.com/yumrepos/azure-cli
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
-sudo dnf install azure-cli
+sudo dnf install azure-cli -y
